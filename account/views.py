d75a6f52ca07b3b9765bfeea302bfc0e9089b2de
YGJ <1392204404@qq.com>
2018-12-20 23:23:32 +0800

    使用Django认证框架

--- account/views.py
+++ account/views.py
@@ -1,3 +1,26 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
 
 # Create your views here.
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username=cd['用户名'],
+                                password=cd['密码'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('成功认证')
+                else:
+                    return HttpResponse('帐号被禁用')
+            else:
+                return HttpResponse('帐号无效')
+    else:
+        form = LoginForm()
+
+    return render(request, 'account/login.html', {'form': form})
