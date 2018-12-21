0be09fcaabff032387747567e80ee29aeae92da0
YGJ <1392204404@qq.com>
2018-12-21 22:05:39 +0800

    使用Django自带的认证视图（views），创建登录登出界面

--- account/views.py
+++ account/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm
+from  django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -24,3 +25,13 @@ def user_login(request):
         form = LoginForm()
 
     return render(request, 'account/login.html', {'form': form})
+
+# login_required装饰器会检查当前用户是否通过认证
+# 如果用户通过认证，则会执行装饰的视图
+# 如果没有通过认证，会把用户重定向到带有一个名为next的GET参数的登录URL
+# 该GET参数保存的变量为用户当前尝试访问的页面URL
+@login_required
+def dashboard(request):
+    #section变量用来跟踪用户在站点中正在查看的页面
+    return render(request, 'account/dashboard.html', {'secton': 'dashboard'})
+
--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+from django.urls import reverse_lazy
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -122,3 +123,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+LOGIN_REDIRECT_URL = reverse_lazy('dashboard')
+LOGIN_URL = reverse_lazy('login')
+LOGOUT_URL = reverse_lazy('logout')
